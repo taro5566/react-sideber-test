192d96706790cbc5b906467775ec1a6f21c462b7
taro5566 <ryo_taro56@yahoo.cp.jp>
2023-02-16 17:39:54 +0900

    更新

---
+++ src/components/SidebarData.js
@@ -0,0 +1,37 @@
+export const SidebarData = [
+  {
+    title: "ホーム",
+    icon: "1",
+    link: "/home",
+  },
+  {
+    title: "メール",
+    icon: "2",
+    link: "/mail",
+  },
+  {
+    title: "アナリティクス",
+    icon: "3",
+    link: "/analitics",
+  },
+  {
+    title: "友達追加",
+    icon: "4",
+    link: "/friends",
+  },
+  {
+    title: "お支払い設定",
+    icon: "5",
+    link: "/payment",
+  },
+  {
+    title: "アップロード",
+    icon: "6",
+    link: "/upload",
+  },
+  {
+    title: "詳細設定",
+    icon: "7",
+    link: "/rocket",
+  },
+];
---
+++ src/components/SidebarIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Icon from './innu.png';
+
+export const SidebarIcon = () => {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} />
+      <p>programming@gmail.com</p>
+    </div>
+  )
+}
